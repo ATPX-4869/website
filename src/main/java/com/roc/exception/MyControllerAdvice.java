3aed4fe4a81d8b2312238ec2de3d05c569f5e6c9
TeacherZhang <2442652360@qq.com>
2021-01-20 10:07:45 +0800

    异常处理完成

--- src/main/java/com/roc/exception/MyControllerAdvice.java
+++ src/main/java/com/roc/exception/MyControllerAdvice.java
@@ -1,18 +1,22 @@
 package com.roc.exception;
 
+import com.roc.common.CommonConstants;
 import com.roc.common.R;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @ResponseBody
 @ControllerAdvice
 public class MyControllerAdvice {
 
     @ExceptionHandler
-    public R myExceptionHandler(MyException e){
-        System.out.println("=============跑起来");
-        return R.failed(e.getCode(),e.getMsg());
+    public R myExceptionHandler(Exception e){
+        log.error(e.getMessage(),e);
+        return R.failed(CommonConstants.FAIL).setMsg(e.getMessage());
     }
 
 }
